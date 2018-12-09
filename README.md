2cecb94b7f3f6301708bbf1ef8ab6a7cf34a2e22
Jimmy <azman740412@gmail.com>
2018-12-09 12:54:46 +0800

    prj structure

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # kabinet-bootstrap
 - date : 10 - 11 December 2018
 - 2 days training at FT training center
-- using Learning Bootstrap 4, 2nd edition as reference book
+- using Learning Bootstrap 4, 2nd editio (packtpub)n as reference book
+
+# History
+09-12-2018
---
+++ chap4/offset.php
@@ -0,0 +1,12 @@
+<?php include '../asset/header.php' ?>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4 my-col">
+      <!-- column 2 //-->
+    </div>
+    <div class="col-md-4 my-col">
+      <!-- column 3 //-->
+    </div>
+  </div>
+</div>
+<?php include '../asset/footer.php' ?>
